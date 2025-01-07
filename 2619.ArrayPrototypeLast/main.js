aac944730cef6328dedd3946553573a624d450dc
jamal rizki <73854356+jamalrizki@users.noreply.github.com>
2025-01-07 17:32:14 -0600

    last

---
+++ 2619.ArrayPrototypeLast/main.js
@@ -0,0 +1,15 @@
+Array.prototype.last = function() {
+
+    if(this.length == 0) {
+        return -1
+    } else {
+    return this[this.length -1]
+    }
+    
+};
+
+const arr = [1, 2, 3, 4];
+console.log(arr.last()); // Outputs: 4
+
+const emptyArr = [];
+console.log(emptyArr.last()); // Outputs: -1
