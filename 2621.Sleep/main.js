dfd319b7d0ddb16cb35598fe064a091917eda55a
jamal rizki <73854356+jamalrizki@users.noreply.github.com>
2025-01-09 16:13:19 -0600

    sleep

---
+++ 2621.Sleep/main.js
@@ -0,0 +1,10 @@
+async function sleep(millis) {
+    return new Promise((res) => {
+     setTimeout(() => res(millis), millis)
+    } )
+ }
+
+ 
+let t = Date.now()
+sleep(100).then(() => console.log(Date.now() - t)) // 100
+
